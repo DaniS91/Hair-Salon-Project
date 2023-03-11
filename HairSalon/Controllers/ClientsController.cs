0133e33b28d3ab98f05ace360c15d6c365c6e164
DaniS91 <114887061+DaniS91@users.noreply.github.com>
2023-03-10 19:54:50 -0800

    Add details action to clients controller

---
+++ HairSalon/Controllers/ClientsController.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using HairSalon.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HairSalon.Controllers
+{
+  public class ClientsController : Controller
+  {
+    private readonly HairSalonContext _db;
+
+    public ClientsController(HairSalonContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Details(int id)
+    {
+      Client thisClient = _db.Clients
+                          .Include(client => client.Stylist)
+                          .FirstOrDefault(client => client.ClientId == id);
+      return View(thisClient);
+    }
+  }
+}
