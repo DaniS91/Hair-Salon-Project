7cfa3fe61de0903c27889c822451a14b28b8eb1f
DaniS91 <114887061+DaniS91@users.noreply.github.com>
2023-03-10 21:50:38 -0800

    add Index action to clients controller

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -16,6 +16,14 @@ namespace HairSalon.Controllers
       _db = db;
     }
 
+    public ActionResult Index()
+    {
+      List<Client> model = _db.Clients
+                                  .Include(client => client.Stylist)
+                                  .ToList();
+      return View(model);
+    }
+
     public ActionResult Details(int id)
     {
       Client thisClient = _db.Clients
@@ -28,6 +36,18 @@ namespace HairSalon.Controllers
     {
       ViewBag.StylistId = new SelectList(_db.Stylists, "StylistId", "Name");
       return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Client client)
+    {
+      if (client.StylistId == 0)
+      {
+        return RedirectToAction("Create");
+      }
+      _db.Clients.Add(client);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
     }
   }
 }
